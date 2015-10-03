136e9d70635669d65f00ed4e3a199c5c90731d4c
allejo <vladimir.jimenez.102@my.csun.edu>
2015-10-03 13:42:28 -0700

    Added addNote() function to PulseProject

--- src/PulseProject.php
+++ src/PulseProject.php
@@ -10,6 +10,10 @@ class PulseProject extends ApiPulse
 
     private $urlSyntax = "%s/%s/%s.json";
 
+    // ================================================================================================================
+    //   Subscribers functions
+    // ================================================================================================================
+
     public function getSubscribers($params = array())
     {
         $url = sprintf($this->urlSyntax, parent::apiEndpoint(), $this->id, "subscribers");
@@ -17,6 +21,57 @@ class PulseProject extends ApiPulse
         return parent::fetchJsonArrayToObjectArray($url, "PulseUser", $params);
     }
 
+    // ================================================================================================================
+    //   Notes functions
+    // ================================================================================================================
+
+    /**
+     * Create a new note in this project
+     *
+     * @param  string   $title         The title of the note
+     * @param  string   $content       The body of the note
+     * @param  bool     $owners_only   Set to true if only pulse owners can edit this note.
+     * @param  int|null $user_id       The id of the user to be marked as the note’s last updater
+     * @param  bool     $create_update Indicates whether to create an update on the pulse notifying subscribers on the
+     *                                 changes (required user_id to be set).
+     *
+     * @since  0.1.0
+     *
+     * @return PulseNote
+     */
+    public function addNote($title, $content, $owners_only = false, $user_id = null, $create_update = false)
+    {
+        $url = sprintf($this->urlSyntax, parent::apiEndpoint(), $this->id, "notes");
+        $postParams = array(
+            "id"            => $this->id,
+            "title"         => $title,
+            "content"       => $content,
+            "owners_only"   => $owners_only,
+            "create_update" => $create_update
+        );
+
+        if (!is_null($user_id))
+        {
+            $postParams["user_id"] = $user_id;
+        }
+
+        if ($create_update && is_null($user_id))
+        {
+            throw new \InvalidArgumentException("The user_id value must be set if an update is to be created");
+        }
+
+        $noteResult = self::sendPost($url, $postParams);
+
+        return (new PulseNote($noteResult));
+    }
+
+    /**
+     * Return all of the notes belonging to this project
+     *
+     * @since  0.1.0
+     *
+     * @return PulseNote[]
+     */
     public function getNotes()
     {
         $url = sprintf($this->urlSyntax, parent::apiEndpoint(), $this->id, "notes");
@@ -24,12 +79,20 @@ class PulseProject extends ApiPulse
         return parent::fetchJsonArrayToObjectArray($url, "PulseNote");
     }
 
+    // ================================================================================================================
+    //   Updates functions
+    // ================================================================================================================
+
     public function getUpdates()
     {
         $url = sprintf($this->urlSyntax, parent::apiEndpoint(), $this->id, "updates");
 
         return parent::fetchJsonArrayToObjectArray($url, "PulseUpdate");
     }
+
+    // ================================================================================================================
+    //   Static functions
+    // ================================================================================================================
 
     public static function getPulses($params = array())
     {
