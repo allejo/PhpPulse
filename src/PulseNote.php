6b12c543799bbbafcc54f77b3352422e0eb461fe
allejo <vladimir.jimenez.102@my.csun.edu>
2015-10-03 19:33:14 -0700

    Only allow creation of notes from JSON objects and function structure

--- src/PulseNote.php
+++ src/PulseNote.php
@@ -6,5 +6,18 @@ use allejo\DaPulse\Objects\ApiNote;
 
 class PulseNote extends ApiNote
 {
+    public function __construct($array)
+    {
+        if (!is_array($array))
+        {
+            throw new \InvalidArgumentException("A PulseNote cannot be fetched from an ID.");
+        }
 
+        parent::__construct($array);
+    }
+
+    public function updateNote ($title = null, $content = null, $user_id = null, $create_update = null)
+    {
+
+    }
 }
