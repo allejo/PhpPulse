70f8622f39818cca4181bc8dbe17c1e054a8d9d3
allejo <vladimir.jimenez.102@my.csun.edu>
2015-10-16 06:34:03 -0700

    Created PulseBoard class

---
+++ src/PulseBoard.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace allejo\DaPulse;
+
+use allejo\DaPulse\Objects\ApiBoard;
+
+class PulseBoard extends ApiBoard
+{
+    const API_PREFIX = "boards";
+
+    // ================================================================================================================
+    //   Static functions
+    // ================================================================================================================
+
+    public static function createBoard ($user_id, $name, $description = null)
+    {
+        $url = sprintf("%s.json", parent::apiEndpoint());
+        $postParams = array(
+            "user_id" => $user_id,
+            "name"    => $name
+        );
+
+        self::setIfNotNull($postParams, "description", $description);
+
+        $boardResult = self::sendPost($url, $postParams);
+
+        return (new PulseBoard($boardResult));
+    }
+
+    public static function getBoards ($params = array())
+    {
+        $url = sprintf("%s.json", parent::apiEndpoint());
+
+        return parent::fetchJsonArrayToObjectArray($url, "PulseBoard", $params);
+    }
+}
