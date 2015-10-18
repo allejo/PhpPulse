1ed30be51148cd64e9c5f12ac6b2c5f1f44e41bd
allejo <vladimir.jimenez.102@my.csun.edu>
2015-10-17 22:36:42 -0700

    Partial random push

---
+++ src/PulseColumn.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace allejo\DaPulse;
+
+use allejo\DaPulse\Objects\ApiObject;
+
+class PulseColumn extends ApiObject
+{
+    protected $id;
+    protected $title;
+    protected $type;
+    protected $empty_text;
+    protected $labels;
+}
