f89b6c6c4cb97284dc11f2aa3a153272542b6082
allejo <vladimir.jimenez.102@my.csun.edu>
2015-10-17 19:49:16 -0700

    Added support for managing groups in Pulse boards

--- src/PulseBoard.php
+++ src/PulseBoard.php
@@ -3,10 +3,52 @@
 namespace allejo\DaPulse;
 
 use allejo\DaPulse\Objects\ApiBoard;
+use allejo\DaPulse\Utilities\ArrayUtilities;
 
 class PulseBoard extends ApiBoard
 {
     const API_PREFIX = "boards";
+
+    private $groupsFetched = false;
+
+    public function createGroup ($title)
+    {
+        $url = sprintf("%s/%s/groups.json", parent::apiEndpoint(), $this->getId());
+        $postParams = array(
+            "title" => $title
+        );
+
+        // The API doesn't return the board ID, so since we have access to it here: set it manually
+        $groupResult = self::sendPost($url, $postParams);
+        $groupResult["board_id"] = $this->id;
+
+        return (new PulseGroup($groupResult));
+    }
+
+    public function getGroups ($showArchived = false)
+    {
+        if (!$this->groupsFetched)
+        {
+            $fetchedGroups = $this->fetchGroups($showArchived);
+
+            $this->groups = ArrayUtilities::array_merge_recursive_distinct($this->groups, $fetchedGroups);
+        }
+
+        self::lazyArray($this->groups, "PulseGroup");
+
+        return $this->groups;
+    }
+
+    private function fetchGroups ($showArchived)
+    {
+        $url = sprintf("%s/%s/groups.json", parent::apiEndpoint(), $this->getId());
+
+        $this->groupsFetched = true;
+
+        return self::sendGet($url, array(
+            "show_archived" => $showArchived
+        ));
+    }
 
     public function deleteBoard ()
     {
---
+++ src/PulseGroup.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace allejo\DaPulse;
+
+use allejo\DaPulse\Exceptions\InvalidObjectException;
+use allejo\DaPulse\Objects\ApiObject;
+use Symfony\Component\Config\Definition\Exception\Exception;
+
+class PulseGroup extends ApiObject
+{
+    const API_PREFIX = "boards";
+
+    protected $id;
+    protected $pos;
+    protected $title;
+    protected $color;
+    protected $board_id;
+    protected $archived;
+    protected $deleted;
+
+    public function __construct ($array)
+    {
+        $this->arrayConstructionOnly = true;
+
+        parent::__construct($array);
+    }
+
+    public function getId ()
+    {
+        return $this->id;
+    }
+
+    public function getTitle ()
+    {
+        return $this->title;
+    }
+
+    public function getColor ()
+    {
+        return $this->color;
+    }
+
+    public function getBoardId ()
+    {
+        return $this->board_id;
+    }
+
+    public function isArchived ()
+    {
+        return $this->archived;
+    }
+
+    public function isDeleted ()
+    {
+        return $this->deleted;
+    }
+
+    /**
+     * @throws InvalidObjectException The board ID is nonexistent. This typically occurs when a group has been archived
+     *                                or deleted and the information is not accessible.
+     */
+    public function deleteGroup ()
+    {
+        if (!isset($this->board_id))
+        {
+            throw new InvalidObjectException("This group may have been archived or deleted; its parent board ID cannot be accessed", 3);
+        }
+
+        $this->checkInvalid();
+
+        $url = sprintf("%s/%s/groups/%s.json", parent::apiEndpoint(), $this->getBoardId(), $this->getId());
+
+        self::sendDelete($url);
+
+        $this->deletedObject = true;
+    }
+}
