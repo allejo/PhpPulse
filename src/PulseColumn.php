228c838fab0bd9df5bc3f00f67e3d96faa5a7315
allejo <vladimir.jimenez.102@my.csun.edu>
2015-10-21 13:48:33 -0700

    Support editing and deleting columns

--- src/PulseColumn.php
+++ src/PulseColumn.php
@@ -6,6 +6,8 @@ use allejo\DaPulse\Objects\ApiObject;
 
 class PulseColumn extends ApiObject
 {
+    const API_PREFIX = "boards";
+
     const Date   = "date";
     const Person = "person";
     const Status = "status";
@@ -16,6 +18,7 @@ class PulseColumn extends ApiObject
     protected $type;
     protected $empty_text;
     protected $labels;
+    protected $board_id;
 
     public function getId ()
     {
@@ -40,5 +43,47 @@ class PulseColumn extends ApiObject
     public function getLabels ()
     {
         return $this->labels;
+    }
+
+    public function getBoardId ()
+    {
+        return $this->board_id;
+    }
+
+    public function editTitle ($title)
+    {
+        $this->editField("title", $title);
+    }
+
+    public function editLabels ($labels)
+    {
+        $this->editField("labels", $labels);
+    }
+
+    public function deleteColumn ()
+    {
+        $this->checkInvalid();
+
+        self::sendDelete($this->getColumnsUrl());
+
+        $this->deletedObject = true;
+    }
+
+    private function editField ($field, $value)
+    {
+        $this->checkInvalid();
+
+        $postParams = array(
+            $field => $value
+        );
+
+        self::sendPut($this->getColumnsUrl(), $postParams);
+
+        $this->$field = $value;
+    }
+
+    private function getColumnsUrl ()
+    {
+        return sprintf("%s/%d/columns/%s.json", parent::apiEndpoint(), $this->getBoardId(), $this->getId());
     }
 }
