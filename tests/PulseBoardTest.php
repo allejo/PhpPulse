047ef4a2780defec3580d99b24cbfecb9abc1587
allejo <vladimir.jimenez.102@my.csun.edu>
2015-10-28 19:25:59 -0700

    Rename tests to specify the get operation

--- tests/PulseBoardTest.php
+++ tests/PulseBoardTest.php
@@ -21,69 +21,69 @@ class PulseBoardTest extends PulseUnitTest
         $this->board = new PulseBoard($this->id);
     }
 
-    public function testBoardObjectNotNull()
+    public function testGetBoardObjectNotNull()
     {
         $this->assertNotNull($this->board);
     }
 
-    public function testBoardUrl()
+    public function testGetBoardUrl()
     {
         $expectedURL = "https://phppulse.dapulse.com/boards/" . $this->id;
         $this->assertEquals($expectedURL, $this->board->getUrl());
     }
 
-    public function testBoardId()
+    public function testGetBoardId()
     {
         $this->assertEquals($this->id, $this->board->getId());
     }
 
-    public function testBoardName()
+    public function testGetBoardName()
     {
         $expectedValue = "Static Pulse Board";
         $this->assertEquals($expectedValue, $this->board->getName());
     }
 
-    public function testBoardDescription()
+    public function testGetBoardDescription()
     {
         $expectedValue = "This is a static PulseBoard that PhpPulse will look for in its unit tests but will NOT modify.";
         $this->assertEquals($expectedValue, $this->board->getDescription());
     }
 
-    public function testBoardColumnsNotNull()
+    public function testGetBoardColumnsNotNull()
     {
         $this->assertNotNull($this->board->getColumns());
     }
 
-    public function testBoardColumnsCount()
+    public function testGetBoardColumnsCount()
     {
         $this->assertCountGreaterThan(0, $this->board->getColumns());
     }
 
-    public function testBoardColumnsType()
+    public function testGetBoardColumnsType()
     {
         $column = $this->board->getColumns();
 
         $this->assertPulseObjectType("PulseColumn", $column[0]);
     }
 
-    public function testBoardGroupsNotNull()
+    public function testGetBoardGroupsNotNull()
     {
         $this->assertNotNull($this->board->getGroups());
     }
 
-    public function testBoardGroupType()
+    public function testGetBoardGroupType()
     {
         $group = $this->board->getGroups();
 
         $this->assertPulseObjectType("PulseGroup", $group[0]);
     }
 
-    public function testCreatedAt()
+    public function testGetBoardCreatedAt()
     {
         $this->assertInstanceOf("DateTime", $this->board->getCreatedAt());
     }
 
-    public function testUpdatedAt()
+    public function testGetBoardUpdatedAt()
     {
         $this->assertInstanceOf("DateTime", $this->board->getUpdatedAt());
     }
