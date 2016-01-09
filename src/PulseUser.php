bf6d57dc97a1d76dc0c68ecec603e2e9420e20e7
allejo <allejo@me.com>
2016-01-08 21:47:33 -0800

    Move PulseUser to single file

--- src/PulseUser.php
+++ src/PulseUser.php
@@ -10,7 +10,7 @@
 namespace allejo\DaPulse;
 
 use allejo\DaPulse\Exceptions\IllegalAccessException;
-use allejo\DaPulse\Objects\ApiUser;
+use allejo\DaPulse\Objects\ApiObject;
 
 /**
  * The PulseUser class contains all of the functions related to accessing information about a user
@@ -18,12 +18,117 @@ use allejo\DaPulse\Objects\ApiUser;
  * @package allejo\DaPulse
  * @since   0.1.0
  */
-class PulseUser extends ApiUser
+class PulseUser extends ApiObject
 {
     /**
      * {@inheritdoc}
      */
     const API_PREFIX = "users";
+
+    /**
+     * The resource's URL.
+     *
+     * @var string
+     */
+    protected $url;
+
+    /**
+     * The user's unique identifier.
+     *
+     * @var int
+     */
+    protected $id;
+
+    /**
+     * The user's name.
+     *
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * The user's email.
+     *
+     * @var string
+     */
+    protected $email;
+
+    /**
+     * The user's photo_url.
+     *
+     * @var string
+     */
+    protected $photo_url;
+
+    /**
+     * The user's title.
+     *
+     * @var string
+     */
+    protected $title;
+
+    /**
+     * The user's position.
+     *
+     * @var string
+     */
+    protected $position;
+
+    /**
+     * The user's phone.
+     *
+     * @var string
+     */
+    protected $phone;
+
+    /**
+     * The user's location.
+     *
+     * @var string
+     */
+    protected $location;
+
+    /**
+     * The user's status.
+     *
+     * @var string
+     */
+    protected $status;
+
+    /**
+     * The user's birthday.
+     *
+     * @var string
+     */
+    protected $birthday;
+
+    /**
+     * True if the user is guest, false otherwise
+     *
+     * @var bool
+     */
+    protected $is_guest;
+
+    /**
+     * The user's skills.
+     *
+     * @var string[]
+     */
+    protected $skills;
+
+    /**
+     * Creation time.
+     *
+     * @var \DateTime
+     */
+    protected $created_at;
+
+    /**
+     * Last update time.
+     *
+     * @var \DateTime
+     */
+    protected $updated_at;
 
     /**
      * The membership type of this user with respect to a specific Pulse or PulseBoard. This value will not be set if
@@ -40,8 +145,166 @@ class PulseUser extends ApiUser
      */
     private $urlSyntax = "%s/%s/%s.json";
 
+    // ================================================================================================================
+    //   Getter functions
+    // ================================================================================================================
+
     /**
+     * The resource's URL.
      *
+     * @return string
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * The user's unique identifier.
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * The user's name.
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * The user's email.
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * The user's photo_url.
+     *
+     * @return string
+     */
+    public function getPhotoUrl()
+    {
+        return $this->photo_url;
+    }
+
+    /**
+     * The user's title.
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * The user's position.
+     *
+     * @return string
+     */
+    public function getPosition()
+    {
+        return $this->position;
+    }
+
+    /**
+     * The user's phone.
+     *
+     * @return string
+     */
+    public function getPhone()
+    {
+        return $this->phone;
+    }
+
+    /**
+     * The user's location.
+     *
+     * @return string
+     */
+    public function getLocation()
+    {
+        return $this->location;
+    }
+
+    /**
+     * The user's status.
+     *
+     * @return string
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * The user's birthday.
+     *
+     * @return string
+     */
+    public function getBirthday()
+    {
+        return $this->birthday;
+    }
+
+    /**
+     * True if the user is guest, false otherwise
+     *
+     * @return bool
+     */
+    public function getIsGuest()
+    {
+        return $this->is_guest;
+    }
+
+    /**
+     * The user's skills.
+     *
+     * @return string[]
+     */
+    public function getSkills()
+    {
+        return $this->skills;
+    }
+
+    /**
+     * Creation time.
+     *
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->created_at;
+    }
+
+    /**
+     * Last update time.
+     *
+     * @return \DateTime
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updated_at;
+    }
+
+    /**
+     * Get the membership level of this user with respect to the object that returned or created this instance.
+     *
+     * @throws IllegalAccessException This user was not created by an object which supports memberships
+     *
+     * @returns string The membership access; e.g. "admin", "subscriber"
      */
     public function getMembership ()
     {
