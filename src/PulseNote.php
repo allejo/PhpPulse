246d2f811e0769085c72c29753e95d5f84dd14c9
allejo <allejo@me.com>
2016-01-09 22:46:46 -0800

    Get rid of the ApiNote "partial" class

--- src/PulseNote.php
+++ src/PulseNote.php
@@ -2,11 +2,71 @@
 
 namespace allejo\DaPulse;
 
-use allejo\DaPulse\Objects\ApiNote;
+use allejo\DaPulse\Objects\ApiObject;
 
-class PulseNote extends ApiNote
+class PulseNote extends ApiObject
 {
     const API_PREFIX = "pulses";
+
+    // ================================================================================================================
+    //   Instance Variables
+    // ================================================================================================================
+
+    /**
+     * The collaboration box type (rich_text, file_list, faq_list).
+     *
+     * @var string
+     */
+    protected $type;
+
+    /**
+     * The note's id.
+     *
+     * @var string
+     */
+    protected $id;
+
+    /**
+     * The note's title.
+     *
+     * @var string
+     */
+    protected $title;
+
+    /**
+     * The note's project_id.
+     *
+     * @var string
+     */
+    protected $project_id;
+
+    /**
+     * Describes who can edit this note. Can be either 'everyone' or 'owners'.
+     *
+     * @var string
+     */
+    protected $permissions;
+
+    /**
+     * The note's body
+     *
+     * @var string
+     */
+    protected $content;
+
+    /**
+     * Creation time.
+     *
+     * @var \DateTime
+     */
+    protected $created_at;
+
+    /**
+     * Last update time.
+     *
+     * @var \DateTime
+     */
+    protected $updated_at;
 
     public function __construct ($array)
     {
@@ -14,6 +74,94 @@ class PulseNote extends ApiNote
 
         parent::__construct($array);
     }
+
+    // ================================================================================================================
+    //   Getter functions
+    // ================================================================================================================
+
+    /**
+     * The collaboration box type (rich_text, file_list, faq_list).
+     *
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * The note's id.
+     *
+     * @return string
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * The note's title.
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * The note's project_id.
+     *
+     * @return string
+     */
+    public function getProjectId()
+    {
+        return $this->project_id;
+    }
+
+    /**
+     * Describes who can edit this note. Can be either 'everyone' or 'owners'.
+     *
+     * @return string
+     */
+    public function getPermissions()
+    {
+        return $this->permissions;
+    }
+
+    /**
+     * The note's body.
+     *
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * Creation time.
+     *
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->created_at;
+    }
+
+    /**
+     * Last update time.
+     *
+     * @return \DateTime
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updated_at;
+    }
+
+    // ================================================================================================================
+    //   Modification functions
+    // ================================================================================================================
 
     public function editNote ($title = NULL, $content = NULL, $user_id = NULL, $create_update = NULL)
     {
