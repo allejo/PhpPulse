ff83e755ccd152fc163de7848cd4d0a297355e94
allejo <vladimir.jimenez.102@my.csun.edu>
2016-10-17 10:02:48 -0700

    Handle API change where null user is not null
    This is why you don't change the return types of your API, kids. DaPulse now
    returns an invalid user if no user is assigned to a "person" column.

--- src/Objects/PulseColumnPersonValue.php
+++ src/Objects/PulseColumnPersonValue.php
@@ -63,4 +63,19 @@ class PulseColumnPersonValue extends PulseColumnValue
 
         $this->column_value = new PulseUser($user);
     }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function isNullValue ()
+    {
+        // Thank you very much, DaPulse. Changing the return type of an API endpoint clearly does not break any existing
+        // code. Check to see if an invalid user is returned to be able to return null
+
+        $isNullUser = array_key_exists('value', $this->jsonResponse) &&
+                      array_key_exists('id', $this->jsonResponse['value']) &&
+                      $this->jsonResponse['value']['id'] === 0;
+
+        return (parent::isNullValue() || $isNullUser);
+    }
 }
