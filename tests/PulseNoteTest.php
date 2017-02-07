384143b0f3a8a180f66ae9437d124e98cff74538
allejo <vladimir.jimenez.102@my.csun.edu>
2017-02-07 10:24:59 -0800

    Fix notes for PulseNotes

---
+++ tests/PulseNoteTest.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace allejo\DaPulse\Tests;
+
+use allejo\DaPulse\Exceptions\InvalidObjectException;
+use allejo\DaPulse\Pulse;
+use allejo\DaPulse\PulseNote;
+
+class PulseNoteTest extends PulseUnitTest
+{
+    static $title = "Violently Making Toast";
+    static $content = "By adding the word 'violently' in front a phrase, it makes the phrase more amusing";
+
+    /**
+     * @var Pulse
+     */
+    private $pulse;
+
+    public function setUp ()
+    {
+        parent::setUp();
+
+        $this->pulse = new Pulse(27345095, true);
+    }
+
+    public function testCreateNoteWithUpdateButWithoutUser ()
+    {
+        $this->setExpectedException(\InvalidArgumentException::class);
+
+        $this->pulse->addNote(self::$title, self::$content, true, NULL, true);
+    }
+
+    public function testCreateNote ()
+    {
+        $note = $this->pulse->addNote(self::$title, self::$content, true);
+
+        $this->assertEquals('owners', $note->getPermissions());
+        $this->assertEquals(self::$title, $note->getTitle());
+        $this->assertEquals(self::$content, $note->getContent());
+        $this->assertEquals('rich_text', $note->getType());
+        $this->assertInstanceOf(\DateTime::class, $note->getCreatedAt());
+        $this->assertInstanceOf(\DateTime::class, $note->getUpdatedAt());
+
+        return $note;
+    }
+
+    /**
+     * @depends testCreateNote
+     * @param   PulseNote $note
+     * @return  PulseNote
+     */
+    public function testEditNote ($note)
+    {
+        $newTitle = "My new title";
+
+        $note->editTitle($newTitle);
+        $this->assertEquals($newTitle, $note->getTitle());
+
+        $newContent = "The world that the children made, here!";
+
+        $note->editContent($newContent);
+        $this->assertEquals($newContent, $note->getContent());
+
+        return $note;
+    }
+
+    /**
+     * @depends testEditNote
+     * @param   PulseNote $note
+     */
+    public function testDeletingNote ($note)
+    {
+        $this->setExpectedException(InvalidObjectException::class);
+
+        $note->deleteNote();
+        $note->deleteNote();
+    }
+}
